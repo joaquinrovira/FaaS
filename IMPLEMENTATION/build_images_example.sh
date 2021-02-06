4319ee5876a1dddf11e5864e2a48cfe7f174b417
Ximo Rovira <joaquin.rovira97@gmail.com>
2021-02-06 17:58:23 +0100

    Add cue binary

--- IMPLEMENTATION/build_images_example.sh
+++ IMPLEMENTATION/build_images_example.sh
@@ -1,10 +1,11 @@
 USER="piturriti1"
-VERSION="v14"
+VERSION="v17"
 
 # Copy deployment to Scaler
 rm -r scaler/manifests/*
 cp -r ../DEPLOYMENT/faas-manifests/* scaler/manifests/
 
+# Build each image
 for w in "database" "frontend" "job-queue" "worker" "scaler"; do
     echo -e "\e[36m##\e[32m Building image \e[33mfaas:$w$VERSION\e[32m:\e[0m"
     echo -e "Copying proxy classes."
@@ -13,6 +14,7 @@ for w in "database" "frontend" "job-queue" "worker" "scaler"; do
     docker build -t $USER/faas:$w$VERSION ./$w
 done
 
+# Push each image to docker-hub
 for w in "database" "frontend" "job-queue" "worker" "scaler"; do
     echo -e "\e[36m##\e[35m Pushing image \e[33mfaas:$w$VERSION\e[32m:\e[0m"
     docker push $USER/faas:$w$VERSION
